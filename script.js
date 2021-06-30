c8a93d4f3c47243670a5941b80c3ec4f3ad55c08
Hayanna <hayannaoliveira.ho@gmail.com>
2021-06-30 05:27:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,16 @@ var i = 0;
 var txt = 'Estas pronto para conhecer o seu duo?'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-$(document).ready(function typeWriter() {
+function typeWriter() {
   if (i < txt.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
-});
-	
+}
+
+window.onload = typeWriter();
+
 window.navigator.mediaDevices.getUserMedia({ video: true})
   .then(stream => {
     video.srcObject = stream;
