978fc4dfd581d299a6dd82183a934bbf8fd1438e
Hayanna <hayannaoliveira.ho@gmail.com>
2021-06-30 05:02:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const video = document.querySelector('#myVidPlayer');
 //w-width,h-height
 var w, h;
 canvas.style.display = "none";
+var i = 0;
+var txt = 'Estas pronto para conhecer o seu duo?'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 	
 window.navigator.mediaDevices.getUserMedia({ video: true})
   .then(stream => {
